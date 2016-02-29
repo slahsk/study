c4415c6c2b78df9f68594932b8b4c277ab05b816
khj <khj@SEPGj92315145>
2016-02-29 13:10:52 +0900

    필요 없는거 삭제

--- sb/src/main/java/study/factory/PowerStats.java
+++ sb/src/main/java/study/factory/PowerStats.java
@@ -2,18 +2,14 @@ package study.factory;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import study.TestVO;
 
 public class PowerStats implements Stats<TestVO>{
 	
-	@Autowired
-	private MyDAO dao;
 
 	@Override
 	public List<TestVO> get(TestVO vo) {
-		return dao.select(vo);
+		return null;
 	}
 	
 	
