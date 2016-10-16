af2e955d33bf5ba5a76418b376750e6404e616ac
khj <slahsk12@gmail.com>
2016-10-16 10:33:29 +0900

    gitTest

--- react/app/Greeter.js
+++ react/app/Greeter.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react'
 import config from './Config.json';
 
-class Greeter extends Component{
-  render(){
-    return (
-      <div>
-        {config.greetText}
-      </div>
-    );
-  }
+class Greeter extends Component {
+    render() {
+        return (
+            <div>
+                {config.greetText}
+            </div>
+        );
+    }
 
 }
 
--- react/app/main.js
+++ react/app/main.js
@@ -1,5 +1,6 @@
-import react from 'react';
+import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './Greeter';
 
-render(<Greeter/>, document.getElementById('root'));
+render(
+    <Greeter/>, document.getElementById('root'));
