526c9736c90bb76b9277a8ed51d8aaaef71500c3
slahsk <slahsk12@gmail.com>
2016-10-23 22:40:07 +0900

    add click event

--- react/app/Card.js
+++ react/app/Card.js
@@ -3,16 +3,30 @@ import CheckList from './CheckList';
 import style from './kanban.css';
 
 class Card extends Component{
+  constructor(){
+    super(...arguments);
+    this.state = {
+      showDetails : false
+    };
+  }
+
   render(){
-    return (
-      <div className={style.card}>
-        <div className={style.card_title}>
-          {this.props.title}
-        </div>
+    let cardDetails;
+    if(this.state.showDetails){
+      cardDetails = (
         <div className={style.card_details}>
           {this.props.description}
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
+      );
+    }
+
+    return (
+      <div className={style.card}>
+        <div className={style.card_title} onClick = { () => this.setState({showDetails : !this.state.showDetails})}>
+          {this.props.title}
+        </div>
+        {cardDetails}
       </div>
     );
   }
