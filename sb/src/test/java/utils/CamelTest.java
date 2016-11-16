2533a040260a088c3cbfb4d886460f1fab319bca
김희준 <slahsk12@gmail.com>
2016-11-16 10:05:22 +0900

    Create CamelTest.java

---
+++ sb/src/test/java/utils/CamelTest.java
@@ -0,0 +1,37 @@
+	import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+import lombok.Data;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CamelTest{
+    @Test
+    public void myTest(){
+          String str = "call-forwardingL1l-ways-on";
+          String repStr = "";
+
+          Pattern p = Pattern.compile("\\-[a-z]{1}");
+          Matcher m = p.matcher(str);
+          int cnt = 0;
+          while (m.find()) {
+              repStr = m.replaceFirst(m.group().replaceAll("\\-","").toUpperCase());
+              m = p.matcher(repStr);
+
+              System.out.println("repCnt : " + cnt + " / repStr : " + repStr);
+
+              cnt++;
+          }
+          System.out.println("====== repStr result ======\r\n" + repStr);
+
+
+    }
+  }
